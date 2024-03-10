01d9d4c9177b4fa3a4abf354b6f867d4c2d3293b
aygnnn7 <aygunahmed7@gmail.com>
2024-03-10 02:50:40 +0100

    Add project files.

---
+++ ImageResizer/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ImageResizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pb = new PictureBox();
+            ResizeBtn = new Button();
+            InfoLbl = new Label();
+            panel1 = new Panel();
+            TypeLbl = new Label();
+            TypeCB = new ComboBox();
+            imgNameLbl = new Label();
+            SelectImageBtn = new Button();
+            label1 = new Label();
+            percentageLbl = new Label();
+            percentageTB = new TextBox();
+            SelectImageDialog = new OpenFileDialog();
+            InfoPnl = new Panel();
+            InfoLbl2 = new Label();
+            SaveBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)pb).BeginInit();
+            panel1.SuspendLayout();
+            InfoPnl.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pb
+            // 
+            pb.BackColor = SystemColors.Window;
+            pb.BorderStyle = BorderStyle.Fixed3D;
+            pb.Image = Properties.Resources._default;
+            pb.Location = new Point(28, 36);
+            pb.Name = "pb";
+            pb.Size = new Size(476, 264);
+            pb.SizeMode = PictureBoxSizeMode.CenterImage;
+            pb.TabIndex = 0;
+            pb.TabStop = false;
+            // 
+            // ResizeBtn
+            // 
+            ResizeBtn.Cursor = Cursors.Hand;
+            ResizeBtn.Font = new Font("Constantia", 9F);
+            ResizeBtn.Location = new Point(529, 226);
+            ResizeBtn.Name = "ResizeBtn";
+            ResizeBtn.Size = new Size(229, 34);
+            ResizeBtn.TabIndex = 1;
+            ResizeBtn.Text = "Resize";
+            ResizeBtn.UseVisualStyleBackColor = true;
+            ResizeBtn.Click += ResizeBtn_Click;
+            // 
+            // InfoLbl
+            // 
+            InfoLbl.AutoSize = true;
+            InfoLbl.Font = new Font("Constantia", 9F, FontStyle.Bold);
+            InfoLbl.Location = new Point(24, 95);
+            InfoLbl.Name = "InfoLbl";
+            InfoLbl.Size = new Size(112, 22);
+            InfoLbl.TabIndex = 3;
+            InfoLbl.Text = "Percentage:";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.GradientActiveCaption;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(TypeLbl);
+            panel1.Controls.Add(TypeCB);
+            panel1.Controls.Add(imgNameLbl);
+            panel1.Controls.Add(SelectImageBtn);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(InfoLbl);
+            panel1.Controls.Add(percentageLbl);
+            panel1.Controls.Add(percentageTB);
+            panel1.Location = new Point(529, 36);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(229, 184);
+            panel1.TabIndex = 4;
+            // 
+            // TypeLbl
+            // 
+            TypeLbl.AutoSize = true;
+            TypeLbl.Font = new Font("Constantia", 9F, FontStyle.Bold);
+            TypeLbl.Location = new Point(24, 134);
+            TypeLbl.Name = "TypeLbl";
+            TypeLbl.Size = new Size(55, 22);
+            TypeLbl.TabIndex = 8;
+            TypeLbl.Text = "Type:";
+            // 
+            // TypeCB
+            // 
+            TypeCB.FormattingEnabled = true;
+            TypeCB.Items.AddRange(new object[] { "Sequential", "Parallel" });
+            TypeCB.Location = new Point(85, 128);
+            TypeCB.Name = "TypeCB";
+            TypeCB.Size = new Size(121, 33);
+            TypeCB.TabIndex = 7;
+            TypeCB.Text = "Sequential";
+            // 
+            // imgNameLbl
+            // 
+            imgNameLbl.AutoSize = true;
+            imgNameLbl.Font = new Font("Segoe UI", 8F);
+            imgNameLbl.Location = new Point(99, 18);
+            imgNameLbl.Name = "imgNameLbl";
+            imgNameLbl.Size = new Size(103, 21);
+            imgNameLbl.TabIndex = 7;
+            imgNameLbl.Text = "No image yet";
+            // 
+            // SelectImageBtn
+            // 
+            SelectImageBtn.Font = new Font("Constantia", 9F);
+            SelectImageBtn.Location = new Point(24, 48);
+            SelectImageBtn.Name = "SelectImageBtn";
+            SelectImageBtn.Size = new Size(182, 34);
+            SelectImageBtn.TabIndex = 5;
+            SelectImageBtn.Text = "Select";
+            SelectImageBtn.UseVisualStyleBackColor = true;
+            SelectImageBtn.Click += SelectImageBtn_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Constantia", 9F, FontStyle.Bold);
+            label1.Location = new Point(24, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 22);
+            label1.TabIndex = 6;
+            label1.Text = "Image:";
+            // 
+            // percentageLbl
+            // 
+            percentageLbl.AutoSize = true;
+            percentageLbl.Location = new Point(179, 95);
+            percentageLbl.Name = "percentageLbl";
+            percentageLbl.Size = new Size(27, 25);
+            percentageLbl.TabIndex = 5;
+            percentageLbl.Text = "%";
+            // 
+            // percentageTB
+            // 
+            percentageTB.Font = new Font("Constantia", 9F);
+            percentageTB.Location = new Point(142, 92);
+            percentageTB.Name = "percentageTB";
+            percentageTB.Size = new Size(41, 29);
+            percentageTB.TabIndex = 4;
+            percentageTB.Text = "100";
+            // 
+            // SelectImageDialog
+            // 
+            SelectImageDialog.FileName = "SelectImageDialog";
+            // 
+            // InfoPnl
+            // 
+            InfoPnl.BackColor = SystemColors.Info;
+            InfoPnl.BorderStyle = BorderStyle.FixedSingle;
+            InfoPnl.Controls.Add(InfoLbl2);
+            InfoPnl.Location = new Point(28, 330);
+            InfoPnl.Name = "InfoPnl";
+            InfoPnl.Size = new Size(730, 85);
+            InfoPnl.TabIndex = 5;
+            InfoPnl.Visible = false;
+            
+            // 
+            // InfoLbl2
+            // 
+            InfoLbl2.AutoSize = true;
+            InfoLbl2.Font = new Font("Constantia", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            InfoLbl2.Location = new Point(25, 31);
+            InfoLbl2.Name = "InfoLbl2";
+            InfoLbl2.Size = new Size(0, 22);
+            InfoLbl2.TabIndex = 0;
+            // 
+            // SaveBtn
+            // 
+            SaveBtn.Enabled = false;
+            SaveBtn.Location = new Point(529, 266);
+            SaveBtn.Name = "SaveBtn";
+            SaveBtn.Size = new Size(229, 34);
+            SaveBtn.TabIndex = 6;
+            SaveBtn.Text = "Save Downsized";
+            SaveBtn.UseVisualStyleBackColor = true;
+            SaveBtn.Click += SaveBtn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(SaveBtn);
+            Controls.Add(InfoPnl);
+            Controls.Add(panel1);
+            Controls.Add(pb);
+            Controls.Add(ResizeBtn);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pb).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            InfoPnl.ResumeLayout(false);
+            InfoPnl.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pb;
+        private Button ResizeBtn;
+        private Label InfoLbl;
+        private Panel panel1;
+        private Label percentageLbl;
+        private TextBox percentageTB;
+        private OpenFileDialog SelectImageDialog;
+        private Button SelectImageBtn;
+        private Label label1;
+        private Panel InfoPnl;
+        private Label InfoLbl2;
+        private Button SaveBtn;
+        private Label imgNameLbl;
+        private ComboBox TypeCB;
+        private Label TypeLbl;
+    }
+}
---
+++ ImageResizer/Form1.cs
@@ -0,0 +1,274 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using Microsoft.VisualBasic.Logging;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms.Design;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ImageResizer
+{
+    public partial class Form1 : Form
+    {
+        private static string _logPath = "/logs.txt";
+        public static string? SelectedImageFullPath { get; set; }
+        public static string? SelectedImageName { get; set; }
+        public Bitmap CurrentResizedImage { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Buttons
+        private void ResizeBtn_Click(object sender, EventArgs e)
+        {
+            ClearAndHideInfoPanel();
+
+            double percentage;
+            if (SelectedImageFullPath != null
+                && double.TryParse(percentageTB.Text, out percentage)
+                && percentage >= 10
+                && percentage <= 99)
+            {
+                double scaleValue = percentage / 100.0;
+                Bitmap originalImage = new Bitmap(pb.Image);
+
+                //just for measuring the performance
+                string result = MeasurePerformance(originalImage, scaleValue, ResizeWay.Parallel);
+                RecordMeasurments(result);
+
+                CurrentResizedImage = ResizeImageParallel(originalImage, scaleValue);
+
+                pb.Image = CurrentResizedImage;
+
+                SaveBtn.Enabled = true;
+                ShowMessageInfoPanel($"Image '{SelectedImageName}' is resized succesfully to {percentage}%", MessageType.Success);
+            }
+            else
+            {
+                ShowMessageInfoPanel("Please make sure that an image is selected and a percentage is entered between 10 and 99", MessageType.Error);
+            }
+        }
+
+        private void SelectImageBtn_Click(object sender, EventArgs e)
+        {
+            SelectImageDialog.Filter = "Image Files (*.jpg;*.jpeg;*.png;*.gif;*.bmp)|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+            SelectImageDialog.Title = "Select an Image";
+
+            if (SelectImageDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+                    SelectedImageFullPath = SelectImageDialog.FileName;
+                    byte[] imageBytes = File.ReadAllBytes(SelectedImageFullPath);
+                    using (MemoryStream ms = new MemoryStream(imageBytes))
+                    {
+                        pb.Image = System.Drawing.Image.FromStream(ms);
+                    }
+
+                    SelectedImageName = Path.GetFileName(SelectedImageFullPath);
+                    imgNameLbl.Text = SelectedImageName;
+
+                    ShowMessageInfoPanel($"Image {SelectedImageName} is ready for downsising, enter a percentage.", MessageType.Info);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading image: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            using (FileStream fs = new FileStream(SelectedImageFullPath, FileMode.Create, FileAccess.Write))
+            {
+                try
+                {
+                    DialogResult result = MessageBox.Show("The downsised image will overwrite the original. Are you sure?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        CurrentResizedImage.Save(fs, ImageFormat.Jpeg);
+                        SaveBtn.Enabled = false;
+                        ShowMessageInfoPanel($"Downsized image '{SelectedImageName}' is saved succesfully", MessageType.Success);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+        #endregion
+
+        #region Resizing Algorithms 
+        private static Bitmap ResizeImageSequential(Bitmap originalImage, double scaleFactor)
+        {
+            int newWidth = (int)(originalImage.Width * scaleFactor);
+            int newHeight = (int)(originalImage.Height * scaleFactor);
+
+            Bitmap resizedImage = new Bitmap(newWidth, newHeight);
+
+            Rectangle rect = new Rectangle(0, 0, originalImage.Width, originalImage.Height);
+            BitmapData originalData = originalImage.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            BitmapData resizedData = resizedImage.LockBits(new Rectangle(0, 0, newWidth, newHeight), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+            unsafe
+            {
+                byte* originalPtr = (byte*)originalData.Scan0;
+                byte* resizedPtr = (byte*)resizedData.Scan0;
+
+                // Iterate over each pixel in the resized image
+                for (int y = 0; y < newHeight; y++)
+                {
+                    for (int x = 0; x < newWidth; x++)
+                    {
+                        int origX = (int)(x / scaleFactor);
+                        int origY = (int)(y / scaleFactor);
+
+                        byte* originalPixel = originalPtr + (origY * originalData.Stride) + (origX * 4);
+                        byte* resizedPixel = resizedPtr + (y * resizedData.Stride) + (x * 4);
+
+                        // Copy pixel values from original image to resized image
+                        resizedPixel[0] = originalPixel[0]; // Blue
+                        resizedPixel[1] = originalPixel[1]; // Green
+                        resizedPixel[2] = originalPixel[2]; // Red
+                        resizedPixel[3] = originalPixel[3]; // Alpha (transparency)
+                    }
+                }
+            }
+
+            originalImage.UnlockBits(originalData);
+            resizedImage.UnlockBits(resizedData);
+
+            return resizedImage;
+        }
+        public static Bitmap ResizeImageParallel(Bitmap originalImage, double scaleFactor)
+        {
+            int newWidth = (int)(originalImage.Width * scaleFactor);
+            int newHeight = (int)(originalImage.Height * scaleFactor);
+
+            Bitmap resizedImage = new Bitmap(newWidth, newHeight);
+
+            Rectangle rect = new Rectangle(0, 0, originalImage.Width, originalImage.Height);
+            BitmapData originalData = originalImage.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            BitmapData resizedData = resizedImage.LockBits(new Rectangle(0, 0, newWidth, newHeight), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+            unsafe
+            {
+                byte* originalPtr = (byte*)originalData.Scan0;
+                byte* resizedPtr = (byte*)resizedData.Scan0;
+
+                // Parallelize the resizing process using Parallel.For
+                Parallel.For(0, newHeight, y =>
+                {
+                    for (int x = 0; x < newWidth; x++)
+                    {
+                        int origX = (int)(x / scaleFactor);
+                        int origY = (int)(y / scaleFactor);
+
+                        byte* originalPixel = originalPtr + (origY * originalData.Stride) + (origX * 4);
+                        byte* resizedPixel = resizedPtr + (y * resizedData.Stride) + (x * 4);
+
+                        // Copy pixel values from original image to resized image
+                        resizedPixel[0] = originalPixel[0]; // Blue
+                        resizedPixel[1] = originalPixel[1]; // Green
+                        resizedPixel[2] = originalPixel[2]; // Red
+                        resizedPixel[3] = originalPixel[3]; // Alpha (transparency)
+                    }
+                });
+            }
+
+            originalImage.UnlockBits(originalData);
+            resizedImage.UnlockBits(resizedData);
+
+            return resizedImage;
+        }
+        public static string MeasurePerformance(Bitmap originalImage, double scaleFactor, ResizeWay type)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            if (type == ResizeWay.Sequential)
+            {
+                stopwatch.Start();
+                Bitmap resizedImageSequential = ResizeImageSequential(originalImage, scaleFactor);
+                stopwatch.Stop();
+
+                return $"Sequential resizing took: {stopwatch.ElapsedMilliseconds} ms";
+            }
+            else
+            {
+                stopwatch.Start();
+                Bitmap resizedImageParallel = ResizeImageParallel(originalImage, scaleFactor);
+                stopwatch.Stop();
+
+                return $"Parallel resizing took: {stopwatch.ElapsedMilliseconds} ms";
+            }
+        }
+
+        public static void RecordMeasurments(string text)
+        {
+            if (File.Exists(_logPath))
+            {
+                using (StreamWriter writer = File.AppendText(_logPath))
+                {
+                    writer.WriteLine(text);
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = File.CreateText(_logPath))
+                {
+                    writer.WriteLine(text);
+                }
+            }
+        }
+
+        public enum ResizeWay
+        {
+            Sequential,
+            Parallel
+        }
+        #endregion
+
+
+
+
+        #region Information Panel
+        private void ShowMessageInfoPanel(string message, MessageType type)
+        {
+            InfoPnl.Visible = true;
+            InfoLbl2.Text = message;
+            switch (type)
+            {
+                case MessageType.Info:
+                    InfoPnl.BackColor = Color.Orange;
+                    break;
+                case MessageType.Error:
+                    InfoPnl.BackColor = Color.Tomato;
+                    break;
+                case MessageType.Success:
+                    InfoPnl.BackColor = Color.MediumSeaGreen;
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void ClearAndHideInfoPanel()
+        {
+            InfoPnl.Visible = false;
+            InfoLbl2.Text = string.Empty;
+        }
+
+        
+
+        enum MessageType
+        {
+            Info,
+            Error,
+            Success
+        }
+        #endregion
+
+    }
+}
